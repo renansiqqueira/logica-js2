b284596e39be5c43e330295271451dbb3284025b
Renan Siqueira dos Passos <rsiqueira78@gmail.com>
2025-04-08 09:17:45 -0300

    funcoes
    criei funcoes para inserir textos na tela.

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,9 +1,24 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+let numSecreto = gerarNumAleatorio();
+console.log(numSecreto);
+exibirTexto('h1', 'Jogo do número secreto');
+exibirTexto('p', 'Escolha um número entre 1 e 10');
+
+function exibirTexto(tag, texto){
+    let tagEscolhida = document.querySelector(tag);
+    tagEscolhida.innerHTML = texto;
+}
 
 function verificarChute(){
-    console.log('O clique funcionou');
+    let chute = document.querySelector('input').value;
+    console.log(chute == numSecreto);
+}
+
+function gerarNumAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
 }
