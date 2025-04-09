104cf979a78bcfde6fe0667345a178f45b4b4376
Renan Siqueira dos Passos <rsiqueira78@gmail.com>
2025-04-09 14:26:22 -0300

    commit desafio02
    Completei o desafio 2, de funções, do curso 2 de JavaScript.

---
+++ js-curso-2-desafio_2/app.js
@@ -0,0 +1,40 @@
+//EX01
+function exibirSaudacao(){
+    console.log('Olá Mundo!');
+}
+
+//EX02
+function exibirNome(nome){
+    console.log(`Olá, ${nome}!`);
+}
+
+//EX03
+function dobrarN(n){
+    let dobro = n *= 2;
+    return dobro;
+}
+
+//EX04
+function obterMedia(x, y, z){
+    let media = (x + y + z)/3
+    return media;
+}
+
+//EX05
+function obterMaior(x, y){
+    if(x > y){
+        console.log(x)
+        return x;
+    }else{
+        console.log(y)
+        return y;
+    }
+}
+
+//EX06
+function obterQuadradoN(n){
+    //let quadrado = n *= n;
+    let quadrado = Math.pow(n,2);
+    console.log(quadrado);
+    return quadrado;
+}
