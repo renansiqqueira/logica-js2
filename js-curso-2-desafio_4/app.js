50792c17a83d21797e43be122756ac57a203c47b
Renan Siqueira dos Passos <rsiqueira78@gmail.com>
2025-04-17 09:30:51 -0300

    resolvi o desafio 4
    manipulando listas

---
+++ js-curso-2-desafio_4/app.js
@@ -0,0 +1,22 @@
+//EX01
+let listaGenerica = [];
+
+//EX02
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+console.log(linguagensDeProgramacao);
+
+//Ex03
+linguagensDeProgramacao.push('Java', 'Ruby', 'Golang');
+console.log(linguagensDeProgramacao);
+
+//EX04
+let listaNomes = ['Jesus', 'Maria', 'José'];
+console.log(listaNomes[0]);
+
+//EX05
+let listaNomes2 = ['Renan', 'Scooby', 'Valentina'];
+console.log(listaNomes2[1]);
+
+//EX06
+let listaNomes3 = ['Eduarda', 'Giovanna', 'Nicolle'];
+console.log(listaNomes3[2]);
